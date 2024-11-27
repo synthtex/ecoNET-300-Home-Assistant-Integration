5aa327951da61d2069d2c2a61d1c08657326c9a4
synthtex <vasileyy@gmail.com>
2024-11-27 20:24:16 +0300

    Update sensor.py from the VS Code

--- custom_components/econet300/sensor.py
+++ custom_components/econet300/sensor.py
@@ -15,6 +15,7 @@ from homeassistant.const import (
     PERCENTAGE,
     SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
     UnitOfTemperature,
+    UnitOfPower,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
@@ -23,22 +24,30 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .common import Econet300Api, EconetDataCoordinator
 from .const import (
     AVAILABLE_NUMBER_OF_MIXERS,
+    AVAILABLE_NUMBER_OF_ECOSTERS,
     DOMAIN,
     OPERATION_MODE_NAMES,
+    ECOSTER_MODE_NAMES,
     REG_PARAM_PRECICION,
     SERVICE_API,
     SERVICE_COORDINATOR,
 )
-from .entity import EconetEntity, MixerEntity
+from .entity import EconetEntity, MixerEntity, EcosterEntity
 
 _LOGGER = logging.getLogger(__name__)
 
+from enum import StrEnum
 
 @dataclass
 class EconetSensorEntityDescription(SensorEntityDescription):
     """Describes Econet sensor entity."""
 
     process_val: Callable[[Any], Any] = lambda x: x
+
+class UnitOfVolumeFlowRates(StrEnum):
+    """Volume flow rate units."""
+
+    KILOGRAMM_PER_HOUR = "kg/h"
 
 
 SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
@@ -74,6 +83,17 @@ SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
         process_val=lambda x: round(x, 2),
     ),
     EconetSensorEntityDescription(
+        key="tempOpticalSensor",
+        translation_key="temp_Optical_Sensor",
+        name="Flame",
+        icon="mdi:fire",
+        native_unit_of_measurement=PERCENTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.POWER_FACTOR,
+        suggested_display_precision=0,
+        process_val=lambda x: round(x),
+    ),
+    EconetSensorEntityDescription(
         key="tempFeeder",
         translation_key="temp_feeder",
         name="Feeder temperature",
@@ -98,12 +118,12 @@ SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
     EconetSensorEntityDescription(
         key="tempBack",
         translation_key="temp_back",
-        name="Water back temperature ",
+        name="Return temperature ",
         icon="mdi:thermometer",
         native_unit_of_measurement=UnitOfTemperature.CELSIUS,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.TEMPERATURE,
-        process_val=lambda x: round(x, 2),
+        process_val=lambda x: round(x),
     ),
     EconetSensorEntityDescription(
         key="tempCWU",
@@ -114,9 +134,9 @@ SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.TEMPERATURE,
         suggested_display_precision=REG_PARAM_PRECICION["tempCWU"],
-        process_val=lambda x: x,
+        process_val=lambda x: round(x, 1),
     ),
-   EconetSensorEntityDescription(
+    EconetSensorEntityDescription(
         key="tempCWUSet",
         translation_key="temp_cwu_set",
         name="HUW set temperature",
@@ -151,9 +171,13 @@ SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
     ),
     EconetSensorEntityDescription(
         key="boilerPowerKW",
-        name="Boiler output in kW",
+        translation_key="boiler_powerKW",
+        name="Boiler output KW",
+        icon="mdi:gauge",
+        native_unit_of_measurement=UnitOfPower.KILO_WATT,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.POWER,
+        suggested_display_precision=REG_PARAM_PRECICION["boilerPowerKW"],
         process_val=lambda x: x,
     ),
     EconetSensorEntityDescription(
@@ -165,11 +189,14 @@ SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
         process_val=lambda x: round(x, 1),
     ),
-        EconetSensorEntityDescription(
+    EconetSensorEntityDescription(
         key="fuelStream",
-        translation_key="fuel_stream",
-        name="Fuel stream",
+        translation_key="fuel_Stream",
+        name="Fuel Stream",
+        icon="mdi:gauge-low",
+        native_unit_of_measurement=UnitOfVolumeFlowRates.KILOGRAMM_PER_HOUR,
         state_class=SensorStateClass.MEASUREMENT,
+        suggested_display_precision=REG_PARAM_PRECICION["fuelStream"],
         process_val=lambda x: round(x, 1),
     ),
     EconetSensorEntityDescription(
@@ -248,19 +275,29 @@ SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
     EconetSensorEntityDescription(
         key="softVer",
         name="Module ecoNET software version",
+        icon="mdi:raspberry-pi",
         device_class="econet_software_version",
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     EconetSensorEntityDescription(
         key="moduleASoftVer",
         name="Module A version",
+        icon="mdi:raspberry-pi",
         device_class="module_a_software_version",
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     EconetSensorEntityDescription(
         key="moduleBSoftVer",
         name="Module B version",
+        icon="mdi:raspberry-pi",
         device_class="Module_b_software_version",
+        entity_category=EntityCategory.DIAGNOSTIC,
+    ),
+    EconetSensorEntityDescription(
+        key="moduleCSoftVer",
+        name="Module C version",
+        icon="mdi:raspberry-pi",
+        device_class="Module_c_software_version",
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     EconetSensorEntityDescription(
@@ -273,7 +310,15 @@ SENSOR_TYPES: tuple[EconetSensorEntityDescription, ...] = (
     EconetSensorEntityDescription(
         key="moduleLambdaSoftVer",
         name="Module Lambda version",
+        icon="mdi:raspberry-pi",
         device_class="module_lamda_software_version",
+        entity_category=EntityCategory.DIAGNOSTIC,
+    ),
+    EconetSensorEntityDescription(
+        key="moduleEcoSTERSoftVer",
+        name="Module EcoSTERS version",
+        icon="mdi:raspberry-pi",
+        device_class="module_ecoster_software_version",
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     EconetSensorEntityDescription(
@@ -300,16 +345,6 @@ class EconetSensor(SensorEntity):
         self.entity_description = entity_description
         self._attr_native_value = None
 
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        if self.entity_description.translation_key:
-            _LOGGER.debug("Using translation key for sensor: %s", self.entity_description.translation_key)
-            return f"entity.sensor.{self.entity_description.translation_key}"
-        else:
-            _LOGGER.debug("Using name for sensor: %s", self.entity_description.name)
-            return self.entity_description.name
-
     def _sync_state(self, value):
         """Sync state."""
         _LOGGER.debug("Update EconetSensor entity: %s", self.entity_description.name)
@@ -317,7 +352,6 @@ class EconetSensor(SensorEntity):
         self._attr_native_value = self.entity_description.process_val(value)
 
         self.async_write_ha_state()
-
 
 class ControllerSensor(EconetEntity, EconetSensor):
     """class controller."""
@@ -330,7 +364,6 @@ class ControllerSensor(EconetEntity, EconetSensor):
     ):
         """Initialize a new instance of the EconetSensor class."""
         super().__init__(description, coordinator, api)
-
 
 class MixerSensor(MixerEntity, EconetSensor):
     """Mixer sensor class."""
@@ -345,6 +378,18 @@ class MixerSensor(MixerEntity, EconetSensor):
             """Initialize a new instance of the EconetSensor class."""
             super().__init__(description, coordinator, api, idx)
 
+class EcosterSensor(EcosterEntity, EconetSensor):
+    """Mixer sensor class."""
+
+    def __init__(
+            self,
+            description: EconetSensorEntityDescription,
+            coordinator: EconetDataCoordinator,
+            api: Econet300Api,
+            idx: int,
+        ):
+            """Initialize a new instance of the EconetSensor class."""
+            super().__init__(description, coordinator, api, idx)
 
 def can_add(desc: EconetSensorEntityDescription, coordinator: EconetDataCoordinator):
     """Check if it can add the key."""
@@ -368,7 +413,6 @@ def create_controller_sensors(coordinator: EconetDataCoordinator, api: Econet300
             )
 
     return entities
-
 
 def create_mixer_sensors(coordinator: EconetDataCoordinator, api: Econet300Api):
     """Create individual sensor descriptions for mixer sensors."""
@@ -405,7 +449,7 @@ def create_mixer_sensors(coordinator: EconetDataCoordinator, api: Econet300Api):
             process_val=lambda x: x,
         )
 
-        if can_add(description2, coordinator):
+        if can_add(description, coordinator) and can_add(description2, coordinator):
             entities.append(MixerSensor(description2, coordinator, api, i))
         else:
             _LOGGER.debug(
@@ -414,6 +458,65 @@ def create_mixer_sensors(coordinator: EconetDataCoordinator, api: Econet300Api):
             )
     return entities
 
+def create_ecoster_sensors(coordinator: EconetDataCoordinator, api: Econet300Api):
+    """Create individual sensor descriptions for ecoster sensors."""
+    entities = []
+
+    for i in range(1, AVAILABLE_NUMBER_OF_ECOSTERS + 1):
+        description = EconetSensorEntityDescription(
+            key=f"ecoSterTemp{i}",
+            name=f"ecoSTER {i} temperature",
+            translation_key=f"ecoster_temp_{i}",
+            icon="mdi:thermometer",
+            native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+            state_class=SensorStateClass.MEASUREMENT,
+            device_class=SensorDeviceClass.TEMPERATURE,
+            suggested_display_precision=1,
+            process_val=lambda x: round(x, 1),
+        )
+        if can_add(description, coordinator):
+            entities.append(EcosterSensor(description, coordinator, api, i))
+        else:
+            _LOGGER.debug(
+                "Availability key: %s does not exist, entity will not be added",
+                description.key,
+            )
+        description2 = EconetSensorEntityDescription(
+            key=f"ecoSterSetTemp{i}",
+            name=f"EcoSTER {i} set temperature",
+            translation_key=f"ecoster_{i}_set_temp",
+            icon="mdi:thermometer",
+            native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+            state_class=SensorStateClass.MEASUREMENT,
+            device_class=SensorDeviceClass.TEMPERATURE,
+            suggested_display_precision=1,
+            process_val=lambda x: round(x, 1),
+        )
+
+        if can_add(description, coordinator) and can_add(description2, coordinator):
+            entities.append(EcosterSensor(description2, coordinator, api, i))
+        else:
+            _LOGGER.debug(
+                "Availability key: %s does not exist, entity will not be added",
+                description2.key,
+            )
+        description3 = EconetSensorEntityDescription(
+            key=f"ecoSterMode{i}",
+            name=f"EcoSTER {i} Operating mode",
+            translation_key=f"ecoster_{i}_mode",
+            icon="mdi:sync",
+            device_class="DEVICE_CLASS_OPERATION_MODE",  # custom class for boiler status
+        process_val=lambda x: ECOSTER_MODE_NAMES.get(x, "Unknown"),
+        )
+
+        if can_add(description, coordinator) and can_add(description3, coordinator):
+            entities.append(EcosterSensor(description3, coordinator, api, i))
+        else:
+            _LOGGER.debug(
+                "Availability key: %s does not exist, entity will not be added",
+                description3.key,
+            )
+    return entities
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -428,5 +531,6 @@ async def async_setup_entry(
     entities: list[EconetSensor] = []
     entities = entities + create_controller_sensors(coordinator, api)
     entities = entities + create_mixer_sensors(coordinator, api)
+    entities = entities + create_ecoster_sensors(coordinator, api)
 
     return async_add_entities(entities)
