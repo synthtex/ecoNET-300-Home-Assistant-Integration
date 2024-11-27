b01d2d10c0f0418414385a19bfc18fe483d8c45a
synthtex <vasileyy@gmail.com>
2024-11-27 20:23:05 +0300

    Update number.py from the VS project

--- custom_components/econet300/number.py
+++ custom_components/econet300/number.py
@@ -6,40 +6,74 @@ from homeassistant.components.number import (
     NumberDeviceClass,
     NumberEntity,
     NumberEntityDescription,
+    NumberMode,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .api import Limits
+from .api import Limits, Econet300Api
 from .common import Econet300Api, EconetDataCoordinator
-from .const import DOMAIN, SERVICE_API, SERVICE_COORDINATOR
+from .const import DOMAIN, SERVICE_API, SERVICE_COORDINATOR, ENTITY_MIN_VALUE, ENTITY_MAX_VALUE, EDITABLE_PARAMS_MAPPING_TABLE
 from .entity import EconetEntity
+from enum import StrEnum
 
 _LOGGER = logging.getLogger(__name__)
 
 
 @dataclass
 class EconetNumberEntityDescription(NumberEntityDescription):
-    """Describes Econet binary sensor entity."""
+    """Describes Econet number entity."""
+
+class UnitOfVolumeFlowRates(StrEnum):
+    """Volume flow rate units."""
+
+    KILOWATTHOUR_PER_KG = " kWh/kg"
+    KG_PER_HOUR = " kg/h"
 
 
 NUMBER_TYPES: tuple[EconetNumberEntityDescription, ...] = (
     EconetNumberEntityDescription(
-        key="tempCOSet",
-        name="Boiler set temperature",
-        translation_key="temp_co_set",
+        key="CO_TEMP_SET",
+        name="CH boiler preset temperature",
+        translation_key="co_temp_set",
         icon="mdi:thermometer",
         device_class=NumberDeviceClass.TEMPERATURE,
         native_unit_of_measurement=UnitOfTemperature.CELSIUS,
         entity_registry_visible_default=True,
-        min_value=27,
-        max_value=68,
+        min_value=60,
+        max_value=85,
         native_step=1,
     ),
     EconetNumberEntityDescription(
-        key="tempCWUSet",
+        key="CALORIFIC_KWH_KG",
+        name="Fuel caloricity",
+        mode=NumberMode.BOX,
+        translation_key="calorific_set",
+        icon="mdi:gauge-low",
+        device_class=NumberDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfVolumeFlowRates.KILOWATTHOUR_PER_KG,
+        entity_registry_visible_default=True,
+        min_value=0.1,
+        max_value=25,
+        native_step=0.1,
+    ),
+    EconetNumberEntityDescription(
+        key="FUEL_KG_H",
+        name="Fuel feeder speed",
+        mode=NumberMode.BOX,
+        translation_key="fuelfeederspeed_set",
+        icon="mdi:gauge-low",
+        device_class=NumberDeviceClass.VOLUME_FLOW_RATE,
+        native_unit_of_measurement=UnitOfVolumeFlowRates.KG_PER_HOUR,
+        entity_registry_visible_default=True,
+        min_value=0.1,
+        max_value=25,
+        native_step=0.1,
+    ),
+    EconetNumberEntityDescription(
+        key="CWU_SET_TEMP",
         name="HUW set temperature",
         translation_key="temp_cwu_set",
         icon="mdi:thermometer",
@@ -54,7 +88,7 @@ NUMBER_TYPES: tuple[EconetNumberEntityDescription, ...] = (
 
 
 class EconetNumber(EconetEntity, NumberEntity):
-    """Describes Econet binary sensor entity."""
+    """Describes Econet number sensor entity."""
 
     def __init__(
         self,
@@ -67,18 +101,31 @@ class EconetNumber(EconetEntity, NumberEntity):
 
     def _sync_state(self, value):
         """Sync state."""
-
+        map_key = EDITABLE_PARAMS_MAPPING_TABLE.get(self.entity_description.key)
+        
         self._attr_native_value = value
-
-        self._attr_native_min_value = value - 1
-        self._attr_native_max_value = value + 1
+        self._attr_native_min_value = ENTITY_MIN_VALUE.get(map_key)
+        self._attr_native_max_value = ENTITY_MAX_VALUE.get(map_key)
 
         self.async_write_ha_state()
+        self.hass.async_create_task(self.async_set_limits_values())
+
+    async def async_set_limits_values(self):
+        """Async Sync number limits."""
+        limits = await self._api.get_param_limits(self.entity_description.key)
+        if limits is None:
+            _LOGGER.warning(
+                "Cannot add number entity: %s, numeric limits for this entity is None",
+                self.entity_description.key,
+            )
+        else:
+            self._attr_native_min_value = limits.minv
+            self._attr_native_max_value = limits.maxv
+            self.async_write_ha_state()
 
     async def async_set_native_value(self, value: float) -> None:
         """Update the current value."""
-        _LOGGER.debug("Set value: %s", value)
-
+        
         if value == self._attr_native_value:
             return
 
@@ -97,7 +144,7 @@ class EconetNumber(EconetEntity, NumberEntity):
             )
             return
 
-        if not await self._api.set_param(self.entity_description.key, int(value)):
+        if not await self._api.set_param(self.entity_description.key, value):
             _LOGGER.warning("Setting value failed")
             return
 
@@ -112,8 +159,8 @@ def can_add(desc: EconetNumberEntityDescription, coordinator: EconetDataCoordina
 
 def apply_limits(desc: EconetNumberEntityDescription, limits: Limits):
     """Set the native minimum and maximum values for the given entity description."""
-    desc.native_min_value = limits.min
-    desc.native_max_value = limits.max
+    desc.native_min_value = limits.minv
+    desc.native_max_value = limits.maxv
 
 
 async def async_setup_entry(
